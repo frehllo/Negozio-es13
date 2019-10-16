7b255f71baaf5d878e4d11de2cbe2d1ec719de99
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:21:15 +0200

    Add project files.

---
+++ Negozio es13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Negozio_es13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sconto = 0;
+            double costominore = 0;
+            double prezzototale = 0;
+            Console.Write("Quanto costa il primo prodotto? ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Quanto costa il secondo prodotto? ");
+            double b = double.Parse(Console.ReadLine());
+            if(a > b)
+            {
+                sconto = (b/100)*50;
+                costominore = b - sconto;
+                prezzototale = a + costominore;
+                Console.WriteLine($"Importo scontato da pagare : {prezzototale}");
+            }
+            else
+            {
+                sconto = (a / 100) * 50;
+                costominore = a - sconto;
+                prezzototale = b + costominore;
+                Console.WriteLine($"Importo scontato da pagare : {prezzototale}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
